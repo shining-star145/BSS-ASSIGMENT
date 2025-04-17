060cc101a5abd6dcc812ba020d322a57eee1b722
chaitanya294 <kashyapchaitanya1@gmail.com>
2025-04-17 11:27:57 +0530

    Intial commit: Add fortune.py(v1.0) and release workflow

---
+++ fortune.py
@@ -0,0 +1,10 @@
+print("Welcome to Chaitanya's Fortune Teller (21JE0258)")
+response = input("How are you feeling today? (happy/sad/neutral): ")
+
+if(response == "happy") : 
+    print("Your fortune: Great things await for you, Chaitanya! Keep Smiling")
+elif(response == "sad") : 
+    print("Your fortune: Elevate your mood, Chaitanya! You have a exciting day ahead")
+else :
+    print("Your fortune: Heads up, Chaitanya! You may find some exciting news today")
+

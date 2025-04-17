825fbb3ad855517c864ac46eb9c4323319e5a84c
chaitanya294 <kashyapchaitanya1@gmail.com>
2025-04-17 11:36:30 +0530

    Version 1.1 : Add new mood

--- fortune.py
+++ fortune.py
@@ -1,10 +1,12 @@
 print("Welcome to Chaitanya's Fortune Teller (21JE0258)")
-response = input("How are you feeling today? (happy/sad/neutral): ")
+response = input("How are you feeling today? (happy/sad/neutral/stressed): ")
 
 if(response == "happy") : 
     print("Your fortune: Great things await for you, Chaitanya! Keep Smiling")
 elif(response == "sad") : 
     print("Your fortune: Elevate your mood, Chaitanya! You have a exciting day ahead")
+elif(response == "stressed") : 
+    print("Your fortune: Find something to do, Chaitanya! You may find success today")
 else :
     print("Your fortune: Heads up, Chaitanya! You may find some exciting news today")
 
